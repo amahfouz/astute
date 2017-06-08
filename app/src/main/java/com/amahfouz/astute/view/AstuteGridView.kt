91fe910a8096c6500144efccd903697e840f391e
Ayman Mahfouz <amahfouz@webalo.com>
2017-06-08 10:46:53 -0700

    Moved grid adapter to the grid view file.

--- app/src/main/java/com/amahfouz/astute/view/AstuteGridView.kt
+++ app/src/main/java/com/amahfouz/astute/view/AstuteGridView.kt
@@ -6,6 +6,12 @@ import android.util.AttributeSet
 import android.widget.GridView
 import com.amahfouz.astute.model.RecallGridModel
 import android.content.ContentProvider
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AbsListView
+import android.widget.BaseAdapter
+import android.widget.TextView
+import com.amahfouz.astute.R
 
 /**
  * Grid view showing the game board.
@@ -24,13 +30,45 @@ class AstuteGridView @JvmOverloads constructor
         }
 
     init {
-        adapter = Provider(context)
+        adapter = GridContentAdapter()
     }
 
     //
     // Private
     //
 
-    private fun getContentProvider(): ContentProvider?
-         = adapter as? ContentProvider ?: null
+    private fun getContentProvider(): GridContentAdapter?
+         = adapter as? GridContentAdapter ?: null
+
+    private fun getModel(): RecallGridModel? = provider?.getGridModel()
+
+    //
+    // Inner
+    //
+
+    /**
+     * Provides GridView cell content from a RecallGridModel.Provider.
+     */
+    inner class GridContentAdapter() : BaseAdapter() {
+
+        //
+        // BaseAdapter implementation
+        //
+
+        override fun getItem(p0: Int): Any = 0
+        override fun getItemId(p0: Int): Long = 0
+        override fun getCount(): Int = provider.getGridModel().numCells
+
+        override fun getView(index: Int, recycledView: View?, p2: ViewGroup?): View {
+            val res = context.resources
+            val shape = res.getDrawable(R.drawable.circle)
+            val view = TextView(context)
+            val colWidth = provider.model.columnWidth
+            view.layoutParams = AbsListView.LayoutParams(colWidth, colWidth)
+            if (provider.model.get)
+                view.setBackgroundDrawable(shape);
+            return view
+        }
+
+    }
 }
